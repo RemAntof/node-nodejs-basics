ab3e8668d384cbd5676205e9e86f0e7286967f51
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 21:59:59 +0100

    feat: add complition of task zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const compressedFile = path.join(__dirname, 'files','archive.gz');
+    const filePath = path.join(__dirname, 'files','fileToCompress.txt');
+    
+    const readStream = fs.createReadStream(filePath);
+    const gzipStream = zlib.createGzip();
+    const writeStream = fs.createWriteStream(compressedFile);
+    
+    readStream.pipe(gzipStream).pipe(writeStream);
+    
+    
+    writeStream.on('error', (error) => {
+        console.error(`Error compressing file: ${error.message}`);
+    });
 };
 
 await compress();
