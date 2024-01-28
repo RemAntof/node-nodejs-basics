0d2a3e5595eec70eabfcebd7e1826379fbd0e6fa
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 21:00:57 +0100

    feat: add comlition of task hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files','fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('SHA256');
+
+
+        const stream = fs.createReadStream(filePath);
+
+        stream.on('data', (data) => {
+            hash.update(data);
+        });
+
+        stream.on('end', () => {
+            const hashResult = hash.digest('hex');
+            console.log(hashResult)
+        });
 };
 
 await calculateHash();
