b73a85b1e2b32ca1b0b956ef4967a600703e3995
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 19:11:06 +0100

    feat: add complition of task fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const sourceFileName = 'wrongFilename.txt';
+    const targetFileName = 'properFilename.md';
+
+    const sourceFilePath = path.join(__dirname,"files", sourceFileName);
+    const targetFilePath = path.join(__dirname,"files", targetFileName);
+    if (!fs.existsSync(sourceFilePath)) {
+        throw new Error('FS operation failed: Source file does not exist');
+    }
+
+    if (fs.existsSync(targetFilePath)) {
+        throw new Error('FS operation failed: Target file already exists');
+    }
+
+    fs.renameSync(sourceFilePath, targetFilePath);
+
+    console.log('File renamed successfully!');
 };
 
 await rename();
