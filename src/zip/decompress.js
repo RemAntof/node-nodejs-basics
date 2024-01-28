cb1c5bbcc5bdae9b1a35330f4771a7d9332dea72
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 22:02:49 +0100

    feat: add complition zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const compressedFile = path.join(__dirname, 'files','archive.gz');
+    const outputFile = path.join(__dirname, 'files','fileToCompress.txt');
+    
+    const readStream = fs.createReadStream(compressedFile);
+    const gunzipStream = zlib.createGunzip();
+    const writeStream = fs.createWriteStream(outputFile);
+    
+    readStream.pipe(gunzipStream).pipe(writeStream);
 };
 
 await decompress();
