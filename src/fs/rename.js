22837525c5706bc1f5bbdb92d3bb25fdf187b822
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 20:03:26 +0100

    fix: fix fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -9,19 +9,16 @@ const rename = async () => {
     const sourceFileName = 'wrongFilename.txt';
     const targetFileName = 'properFilename.md';
 
-    const sourceFilePath = path.join(__dirname,"files", sourceFileName);
-    const targetFilePath = path.join(__dirname,"files", targetFileName);
-    if (!fs.existsSync(sourceFilePath)) {
-        throw new Error('FS operation failed: Source file does not exist');
-    }
+    const sourceFilePath = path.join(__dirname, 'files', sourceFileName);
+    const targetFilePath = path.join(__dirname, 'files', targetFileName);
 
-    if (fs.existsSync(targetFilePath)) {
-        throw new Error('FS operation failed: Target file already exists');
-    }
-
-    fs.renameSync(sourceFilePath, targetFilePath);
-
+try{
+    await fs.rename(sourceFilePath, targetFilePath);
     console.log('File renamed successfully!');
-};
 
-await rename();
+}catch (err) {
+        throw new Error('FS operation failed: Target file already exists');
+}
+};
+  await rename();
+
