68729d69f15907425f07647a086e41293dc596ea
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 19:15:01 +0100

    feat: add completion of the task fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const fileName = 'fileToRemove.txt';
+    const filePath = path.join(__dirname,'files', fileName);
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error('FS operation failed: File does not exist');
+    }
+
+    fs.unlinkSync(filePath);
+
+    console.log('File deleted successfully!');
 };
 
 await remove();
