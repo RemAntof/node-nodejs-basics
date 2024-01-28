f650a7aa7698b5e323fb38c153f25bc73e3eb755
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 18:50:17 +0100

    feat: add complete task fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const createFile = () => {
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    console.log(filePath);
+    const fileContent = 'I am fresh and young';
+
+    fs.createWriteStream(filePath).write(fileContent);
+
+    if (fs.existsSync(filePath)) {
+        throw new Error('FS operation failed: File already exists');
+    }
+    console.log('File created successfully!');
 };
 
-await create();
+try {
+    createFile();
+} catch (error) {
+    console.error(error.message);
+}
