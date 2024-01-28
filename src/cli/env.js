86192cc4286331f80457ece007d6eb4474d251f1
RemAntof <anton.ramarchuk@gmail.com>
2024-01-28 20:31:43 +0100

    feat: add complition of task cli/args.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,16 +1,8 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const parseEnv = () => {
     try {
-        // Filter environment variables with the prefix RSS_
         const rssEnvVariables = Object.entries(process.env)
             .filter(([key]) => key.startsWith('RSS_'));
 
-        // Print the filtered environment variables in the specified format
         console.log('Filtered environment variables:');
         rssEnvVariables.forEach(([key, value]) => {
             console.log(`RSS_${key}=${value}`);
